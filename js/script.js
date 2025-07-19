8151f2878cf3c3153ecffe05f37ef40f162175a4
RafaelSanzio0 <rafaelsanzio16@gmail.com>
2025-07-19 12:01:06 -0300

    subindo o site

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const checkbox = document.getElementById('consentimento');
+const botaoEnviar = document.getElementById('botaoEnviar');
+
+if (checkbox && botaoEnviar) {
+  checkbox.addEventListener('change', () => {
+    botaoEnviar.disabled = !checkbox.checked;
+  });
+}
+
+const modalForm = document.getElementById('modalForm');
+const formModal = document.getElementById('formModal');
+
+function abrirModal() {
+  modalForm.classList.remove('hidden');
+}
+
+function fecharModal() {
+  modalForm.classList.add('hidden');
+}
+
+if (formModal) {
+  formModal.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const dados = {
+      name: formModal.name.value,
+      email: formModal.email.value,
+      phone: formModal.phone.value
+    };
+    try {
+      await fetch('https://rssl.app.n8n.cloud/webhook-test/send-form', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+      });
+      window.location.href = 'https://hotmart.com/pt-br/marketplace/produtos/mtccontenporaneo/L13395143E?sck=HOTMART_SITE';
+    } catch (error) {
+      alert('Erro ao enviar! Tente novamente.');
+      console.error(error);
+    }
+  });
+}
